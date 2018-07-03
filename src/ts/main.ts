417ba2bb0ef182c6e04fe12b61c20f097cec0347
KMishik <k.mishik@gmail.com>
2018-07-03 17:36:03 +0500

    Рабочая версия

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,17 +2,31 @@
 
   $(document).ready(function(){
     $('.subdiv').on('click', subdivOnClick);
+    $('.month').on('click', monthOnClick);
   })
 
-  function subdivOnClick(event: any){
-    let root: any = '.competition';
+  function subdivOnClick(event: Event){
+    let root: String = '.competition';
     if ($(event.target).attr('class') === 'subdiv__title') {
-      if($(".subdiv__date", this).hasClass('subdiv__date_hidden')) {
-        $(".subdiv__date", root).addClass('subdiv__date_hidden');
-        $(".subdiv__date", this).removeClass('subdiv__date_hidden')
+      if($(".subdiv__date", this).hasClass('list_hidden')) {
+        $(".list", root).addClass('list_hidden');
+        $(".subdiv__date", this).removeClass('list_hidden')
+      } else {
+        $(".list", this).addClass('list_hidden')
       }
+      event.stopPropagation();
     }
   }
 
+  function monthOnClick(event: Event) {
+    let root: String = '.competition';
+    if ($('.list', this).hasClass('list_hidden')) {
+      $('.month__doclinks', root).addClass('list_hidden');
+      $(".month__doclinks", this).removeClass('list_hidden');
+    } else {
+      $(".month__doclinks", this).addClass('list_hidden');
+    }
+    event.stopPropagation();
+  }
 
 }(jQuery);
